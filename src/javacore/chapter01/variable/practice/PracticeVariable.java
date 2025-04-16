ce58dc022f8a01bfdcce5ac61dde07a373efe7b5
demian <damien.ldvdc@gmail.com>
2025-04-15 20:37:15 -0400

    Test Portabilite

--- src/javacore/chapter01/variable/practice/PracticeVariable.java
+++ src/javacore/chapter01/variable/practice/PracticeVariable.java
@@ -3,6 +3,8 @@ package javacore.chapter01.variable.practice;
 public class PracticeVariable {
 
     public static void main(String[] args) {
+        int o = 0;
+        System.out.println(o);
 
     }
 
