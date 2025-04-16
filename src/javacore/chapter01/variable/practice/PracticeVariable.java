c9bd0934e4e8205862fc2124471fb3baf7a689ec
Demian <damien.ldvdc@gmail.com>
2025-04-15 20:18:39 -0400

    Initial Commit

--- src/javacore/chapter01/variable/practice/PracticeVariable.java
+++ src/javacore/chapter01/variable/practice/PracticeVariable.java
@@ -4,12 +4,6 @@ public class PracticeVariable {
 
     public static void main(String[] args) {
 
-        int age = 2;
-
-        int age2 = age;
-        System.out.println(age);
-        System.out.println(age2);
-
     }
 
 }
