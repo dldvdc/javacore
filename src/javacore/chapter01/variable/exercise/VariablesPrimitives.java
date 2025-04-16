cc4a7c9f72f5615e6dc86018d1f151c2e9f38eff
Demian <damien.ldvdc@gmail.com>
2025-04-15 23:06:04 -0400

    Exercice Variables Primitives

---
+++ src/javacore/chapter01/variable/exercise/VariablesPrimitives.java
@@ -0,0 +1,16 @@
+package javacore.chapter01.variable.exercise;
+
+public class VariablesPrimitives {
+    public static void main(String[] args) {
+
+        long isbn = 9782123456789L;
+        short pageCount = 248;
+        float averageRating = 4.2F;
+        boolean isAvailable = true;
+        int totalUsers = 32000;
+        double dueAmount = 112.36;
+        int userId = 85;
+
+
+    }
+}
---
+++ src/javacore/chapter01/variable/practice/PracticeVariablePrimitive.java
@@ -0,0 +1,14 @@
+package javacore.chapter01.variable.practice;
+
+public class PracticeVariablePrimitive {
+
+    public static void main(String[] args) {
+
+        byte age = 1;
+        short age1 = 2;
+
+        float c = 0.123456789123456F;
+        System.out.println(c);
+    }
+
+}
