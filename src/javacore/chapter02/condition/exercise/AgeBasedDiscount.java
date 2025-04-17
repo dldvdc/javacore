b86750d0b8aa704c4259a8c002ffcc8c6cbf64f2
Demian <damien.ldvdc@gmail.com>
2025-04-17 15:35:59 -0400

    AgeBasedDiscount

---
+++ src/javacore/chapter02/condition/exercise/AgeBasedDiscount.java
@@ -0,0 +1,34 @@
+package javacore.chapter02.condition.exercise;
+
+public class AgeBasedDiscount {
+    public static void main(String[] args) {
+
+        //1
+        //2 - Valeurs à tester : 25, 26, 64, 65
+        int age = 22;
+        boolean isYoung = age <= 25;
+        boolean isOld = age >= 65;
+
+        //3
+        //4 - Valeurs à tester : 0, 25.99, -6
+        double price = 25.99;
+
+        //5
+        if (price <= 0) {
+            System.out.println("C'est votre jour de chance ! Aujourd'hui c'est gratuit");
+            price = 0;
+        }
+        else if (isYoung) {
+            System.out.println("Vous bénéficiez d'un Forfait Jeune ! -25% !");
+            price *= 0.75;
+        } else if (isOld) {
+            System.out.println("Vous bénéficiez d'un Forfait Señor ! -75% !");
+            price *= 0.25;
+        }
+        else {
+            System.out.println("Vous ne bénéficiez d'aucune réduction...");
+        }
+
+        System.out.println("Prix de votre billet de train : " + price + "€");
+    }
+}
---
+++ src/javacore/chapter02/condition/exercise/NumberComparison.java
@@ -0,0 +1,21 @@
+package javacore.chapter02.condition.exercise;
+
+public class NumberComparison {
+    public static void main(String[] args) {
+
+        //1
+        int number1 = -456;
+        int number2 = -556;
+        System.out.println("\nLes deux nombres proposés sont : " + number1 + " et " + number2);
+
+        //2
+        if ( number1 > number2 ) {
+            System.out.println("Le nombre " + number1 + " est le plus grand des deux nombres");
+        } else if ( number2 > number1 ) {
+            System.out.println("Le nombre " + number2 + " est le plus grand des deux nombres");
+        } else {
+            System.out.println("Les deux nombres sont égaux, il n'y a donc pas de plus grand");
+        }
+
+    }
+}
