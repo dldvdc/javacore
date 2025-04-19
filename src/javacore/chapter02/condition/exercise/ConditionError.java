d9e0b5161b74b409b775c77784b81013211b2e47
Demian <damien.ldvdc@gmail.com>
2025-04-19 10:53:10 -0400

    ConditionError

---
+++ src/javacore/chapter02/condition/exercise/ConditionError.java
@@ -0,0 +1,32 @@
+package javacore.chapter02.condition.exercise;
+
+public class ConditionError {
+    public static void main(String[] args) {
+
+        int age = 80;
+
+        if(age <= 16) {
+            System.out.println("Vous êtes mineur.");
+        }
+        else if(age == 17) {
+            System.out.println("Vous êtes bientôt majeur !");
+        }
+        else if(age == 18) {
+            System.out.println("Vous êtes majeur.");
+        }
+
+        // Ce bloc de code doit poursuivre la logique des blocs précédents, et opérer comme un filtre via les mêmes opérateurs logiques "==" ou "<".
+        // Donc si d'autres bloc de code sont attendus, il faut limiter par le haut ce bloc-ci avec "<" et non ">" qui condamnerait les blocs suivants à ne jamais être exécutés.
+
+        else if(age < 60) {
+            System.out.println("Vous êtes un adulte.");
+        }
+        else if(age >= 60 && age < 100) {
+            System.out.println("Vous n'êtes plus tout jeune.");
+        }
+        else {
+            System.out.println("Vous êtes une exception dans ce monde !");
+        }
+
+    }
+}
