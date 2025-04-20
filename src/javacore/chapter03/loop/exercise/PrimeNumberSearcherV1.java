62d5f9def614e9fa4044a6f004e736702f970b4d
Demian <damien.ldvdc@gmail.com>
2025-04-20 00:09:37 -0400

    PrimeNumberSearcherV1

---
+++ src/javacore/chapter03/loop/exercise/PrimeNumberSearcherV1.java
@@ -0,0 +1,25 @@
+package javacore.chapter03.loop.exercise;
+
+public class PrimeNumberSearcherV1 {
+
+    public static void main(String[] args) {
+
+        int primeNumberFound = 0;
+
+        for ( int number = 1; number <= 100 && primeNumberFound <= 5; number++ ) {
+
+            int divisorCount = 0;
+
+            for ( int divisor = 2; divisor < number; divisor++ ) {
+                if (number % divisor == 0) {
+                    divisorCount++;
+                }
+            }
+
+            if (divisorCount == 0) {
+                System.out.println(number + " est un nombre premier");
+                primeNumberFound++;
+            }
+        }
+    }
+}
