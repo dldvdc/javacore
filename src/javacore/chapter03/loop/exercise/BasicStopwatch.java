07cf1236b1dc7b9adbd79371c0d462a62388b4a7
Demian <damien.ldvdc@gmail.com>
2025-04-21 16:23:28 -0400

    FullPyramid

---
+++ src/javacore/chapter03/loop/exercise/BasicStopwatch.java
@@ -0,0 +1,41 @@
+package javacore.chapter03.loop.exercise;
+
+public class BasicStopwatch {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        int hours = 0;
+        int minutes = 0;
+
+        int finalTime = 63;
+
+
+            for ( int seconds = 0 ; seconds < finalTime ; seconds++ ) {
+
+                if ( seconds >= 60 ) {
+                    minutes++;
+                    seconds = 0;
+                }
+
+                if ( minutes >= 60 ) {
+                    hours++;
+                    minutes = 0;
+                }
+
+                String displayHours = hours < 10  ? "0" + hours  : "" + hours;
+                System.out.print(displayHours + ":");
+
+                String displayMinutes = minutes < 10  ? "0" + minutes  : "" + minutes;
+                System.out.print(displayMinutes + ":");
+
+                String displaySeconds = seconds < 10  ? "0" + seconds  : "" + seconds;
+                System.out.println(displaySeconds);
+
+                Thread.sleep(1000);
+
+            }
+
+        }
+
+    }
---
+++ src/javacore/chapter03/loop/exercise/FullPyramid.java
@@ -0,0 +1,25 @@
+package javacore.chapter03.loop.exercise;
+
+public class FullPyramid {
+
+    public static void main(String[] args) {
+
+        int numberOfRows = 12;
+
+        for ( int y = 0 ; y < numberOfRows ; y++) {
+
+            for (int space = 0; space < numberOfRows - y ; space++) {
+                System.out.print(" ");
+            }
+
+            for (int star = 0; star < (y * 2 + 1); star++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
