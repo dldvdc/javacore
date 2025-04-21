6d419cdc9197ab473d2a1d04092ae185d9f51771
demian <damien.ldvdc@gmail.com>
2025-04-21 02:29:15 -0400

    ChessboardDrawing

---
+++ src/javacore/chapter03/loop/exercise/ChessboardDrawing.java
@@ -0,0 +1,44 @@
+package javacore.chapter03.loop.exercise;
+
+public class ChessboardDrawing {
+
+    public static void main(String[] args) {
+
+        char whiteChar = '.';
+        char blackChar = '#';
+
+        int chessboardSize = 8;
+
+        boolean blackTurn = true;
+
+        for ( int y = 0 ; y < chessboardSize ; y++) {
+
+            for (int x = 0; x < chessboardSize; x++) {
+
+                if (blackTurn) {
+
+                    System.out.print("  " + blackChar);
+
+                } else {
+
+                    System.out.print("  " + whiteChar);
+
+                }
+
+                blackTurn = !blackTurn;
+
+            }
+
+            System.out.println();
+
+            if ( chessboardSize % 2 == 0 ) {
+
+                blackTurn = !blackTurn;
+
+            }
+
+        }
+
+    }
+
+}
--- src/javacore/chapter03/loop/exercise/FibonacciCalculator.java
+++ src/javacore/chapter03/loop/exercise/FibonacciCalculator.java
@@ -4,7 +4,27 @@ public class FibonacciCalculator {
 
     public static void main(String[] args) {
 
+        long a = 0L;
+        long b = 1L;
 
+        for ( int counter = 0 ; counter < 50 ; counter++ ) {
+
+            if (a % 2 == 0) {
+                System.out.print(a + " (even); ");
+            } else {
+                System.out.print(a + " ; ");
+            }
+
+            if (b % 2 == 0) {
+                System.out.print(b + " (even); ");
+            } else {
+                System.out.print(b + " ; ");
+            }
+
+            a += b;
+            b += a;
+
+        }
 
     }
 
