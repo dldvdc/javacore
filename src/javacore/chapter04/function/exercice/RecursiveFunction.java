812274f07c7b91accc480846717451815a37d0e8
Demian <damien.ldvdc@gmail.com>
2025-04-24 16:55:51 -0400

    RecursiveFunction

---
+++ src/javacore/chapter04/function/exercice/RecursiveFunction.java
@@ -0,0 +1,24 @@
+package javacore.chapter04.function.exercice;
+
+public class RecursiveFunction {
+
+    public static void main(String[] args) {
+
+        displayNumbers(1, 10000);
+
+    }
+
+    public static void displayNumbers(int initialNumber, int endNumber) {
+
+        System.out.println(initialNumber);
+
+        if (initialNumber < endNumber) {
+
+            initialNumber++;
+
+            displayNumbers(initialNumber, endNumber);
+        }
+
+    }
+
+}
