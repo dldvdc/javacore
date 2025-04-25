f8f2afd6f88e44fad4d93b9d71885132b4c8fb36
Demian <damien.ldvdc@gmail.com>
2025-04-24 22:08:02 -0400

    RecursiveFactorial

---
+++ src/javacore/chapter04/function/exercice/RecursiveFactorial.java
@@ -0,0 +1,47 @@
+package javacore.chapter04.function.exercice;
+
+public class RecursiveFactorial {
+    
+    public static void main(String[] args) {
+    
+    displayFactorial(20);
+
+    }
+    
+
+    public static void displayFactorial(int number) {
+
+        long factorial = calculateFactorial(number, 1L);
+
+        if (factorial == -1) {
+
+            System.out.println("\nLa valeur de la factorielle de " + number + " est trop grande pour être stockée dans un type long...\n");
+
+            return;
+
+        }
+
+        System.out.println("\nLa factorielle de " + number + " est " + factorial + "\n");
+
+    }
+
+
+
+    public static long calculateFactorial(int number, long factor) {
+
+        if (factor < 0) return -1;
+
+        else if (number >= 1) {
+
+            factor *= number;
+            number--;
+
+            factor = calculateFactorial( number , factor );
+
+        }
+
+        return factor;
+
+    }
+
+}
