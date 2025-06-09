1ea5ef7ed35000fde72b68872b991195ffd3d546
Demian <damien.ldvdc@gmail.com>
2025-06-09 01:46:32 -0400

    MysteryNumberGame

---
+++ src/javacore/chapter05/object/exercise/MysteryNumberGame.java
@@ -0,0 +1,66 @@
+package javacore.chapter05.object.exercise;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MysteryNumberGame {
+
+    private static final int MIN = 0;
+    private static final int MAX = 100;
+    private static final int userMaxAttempt = 7;
+
+    private final int mysteryNumber;
+
+    public MysteryNumberGame() {
+        Random random = new Random();
+        this.mysteryNumber = random.nextInt((MAX - MIN) + 1) + MIN;
+        launchGame();
+    }
+
+    public static void main(String[] args) {
+
+        MysteryNumberGame game = new MysteryNumberGame();
+
+    }
+
+    private void launchGame() {
+
+        Scanner scanner = new Scanner(System.in);
+        int userAttempt = 0;
+
+        System.out.print("\nCherchez le nombre mystère : ");
+
+        while (userAttempt < userMaxAttempt) {
+
+            int userNumber = Integer.parseInt(scanner.nextLine());
+
+            if (userNumber == mysteryNumber) {
+                System.out.println("Vous avez gagné ! Le nombre mystère était bien " + mysteryNumber);
+                break;
+            }
+            else {
+
+                if (userNumber < mysteryNumber) {
+                    System.out.println("C'est plus grand !");
+                }
+                else {
+                    System.out.println("C'est plus petit !");
+                }
+
+                userAttempt++;
+
+                if (userAttempt == userMaxAttempt) {
+                    System.out.print("Vous avez perdu...");
+                    break;
+                }
+                else {
+                    System.out.println("Il vous reste encore " + (userMaxAttempt - userAttempt) + " essais...\n");
+                }
+
+            }
+
+        }
+
+    }
+
+}
